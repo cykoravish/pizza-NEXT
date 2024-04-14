010788b0f1e65c7e6046f245aaa065bcfa9e792a
Ravish Bisht <ravishbisht85@gmail.com>
2024-04-14 12:56:55 +0530

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1>welcome to pizza hub</h1>
-      <Button variant="outline">Button</Button>
+      <Button>Button</Button>
 
     </main>
   );
